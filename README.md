b85c3b92332e2c0d596d2dcaef5fbcd3ca2a1119
=renaud <=renaud.dufour59@gmail.com>
2014-11-18 12:52:24 +0100

    First version of R script, Readme and CodeBook

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 Human-Activity-Recognition
 ==========================
 
-Course project of the "Getting and Cleaning Data" course from Coursera
+This repository contains material for the course project of the Coursera course "Getting and Cleaning Data", part of the Data Science specialization. The overall aim of this project is to write an R script generating a tidy dataset from raw data, along with a CodeBook describing the variables, the data, and the transformations performed to clean up the data.
+
+The data used for this project are data collected from the accelerometers from the Samsung Galaxy S smartphone. They are provided by the UCI machine learning repository, a full description is available [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+
+## Content
+
+* `run_analysis.R`: the R script
+* `Readme.md`: the actual readme file
+* `CodeBook.md`: the code book describing the variables, data and transformations performed to obtain a tidy dataset
+
+## Pre-requisite
+
+The R script does not include code to download the source data. The latter are described and can be obtained [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones). They can also be downloaded directly from [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+** For the `run_analysis.R` script to run properly, data have to be downloaded and extracted to a folder named "UCI HAR Dataset" within R's working directory.**
+
+## Remarks
+
+* The script only uses the base R package
+* For consistency with the original data description, variables labels have not been renames (although some contain '(', ')' or '-' characters)
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,75 @@
 # run_analysis.R
 #
+# Author    : Rduf59
+# Date      : November, 16, 2014
+# R version : R-3.1.1
 #
+# This script was developped for the course project of "Getting and Cleaning data"
+# on Coursera. It performs the following analysis :
+#   - Merges the training and the test sets to create one data set.
+#   - Extracts only the measurements on the mean and standard deviation. 
+#   - Add descriptive activity names to name the activities in the data set.
+#   - Appropriately labels the data set with descriptive variable names.
+#   - Creates a second, independent tidy data set with the average of each variable
+#     for each activity and each subject.
+#
+# Data are expected to be located in "./UCI HAR Dataset/" in R's working directory.
+#
+# For detailled information regarding the variables, the source data, and the transformations
+# please refer to the Readme.md file.
+
+
+
+# TO DO
+# find a better way for sorting such that row numbers are re-ordered
+# see marque page in firefox ....
+
+
+
+# Read the data, defining variables names
+features.names <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE,
+                             check.names = FALSE, col.names = c("feature id","feature name"))
+activity.label  <- read.table("./UCI HAR Dataset/activity_labels.txt",
+                              col.names = c("activity id","activity name"))
+
+training.subject  <- read.table("./UCI HAR Dataset/train/subject_train.txt",
+                                col.names = "Subject")
+training.activity <- read.table("./UCI HAR Dataset/train/y_train.txt",
+                                col.names = "Activity")
+training.data     <- read.table("./UCI HAR Dataset/train/X_train.txt",
+                                col.names = features.names[,2],
+                                check.names = FALSE)
+
+test.subject  <- read.table("./UCI HAR Dataset/test/subject_test.txt",
+                            col.names = "Subject")
+test.activity <- read.table("./UCI HAR Dataset/test/y_test.txt",
+                            col.names = "Activity")
+test.data     <- read.table("./UCI HAR Dataset/test/X_test.txt",
+                            col.names = features.names[,2],
+                            check.names = FALSE)
+
+training      <- cbind(training.subject,training.activity,training.data)
+test          <- cbind(test.subject,test.activity,test.data)
+
+# Merge the two datasets. Sort by Subject and ACtivity
+merged.data <- rbind(training,test)
+merged.data <- merged.data[order(merged.data$Subject,merged.data$Activity),]
+
+# Extracts measurements on the mean and standard deviation
+mean.or.std.features <- grep("mean|std",features.names[,2], value = TRUE)
+mean.and.std.data     <- merged.data[,c("Subject","Activity",mean.or.std.features)]
+
+# Add descriptive activity names to name the activities in the data set
+
+mean.and.std.data$Activity <- factor(mean.and.std.data$Activity,
+                                     levels = activity.label[,1],
+                                     labels = activity.label[,2])
+
+# Creates a second, independent tidy data set with the average of each variable
+# for each activity and each subject
+
+averaged.data <- aggregate(. ~ Subject + Activity,mean.and.std.data, mean)
+averaged.data <- averaged.data[order(averaged.data$Subject,averaged.data$Activity),]
+
+# Write the final data
+
